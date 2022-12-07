b281f287b7e9628e114c70455660e36475100487
drewsparker <drewsparker@icloud.com>
2022-12-06 22:47:36 -0600

    Updating routes and fixing bugs

--- Develop/server.js
+++ Develop/server.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 // import sequelize connection
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
